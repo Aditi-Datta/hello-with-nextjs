b922c110c723a923c7845b827f7850f1f6e6f155
Aditi-Datta <aditidatta373@gmail.com>
2023-10-07 01:22:17 +0600

    fake data added

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,35 @@
 import Navbar from '../../components/Navbar'
 
-function index() {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return{
+    props: {
+      data,
+    },
+  };
+};
+
+function index({data}) {
   return (
     <>
     <Navbar></Navbar>
+    <div style={{textAlign:"center", }}>
+    
+    
+    {
+      data.map((curElement) => {
+        return(
+          <div key={curElement.id} >
+          <h3>{curElement.id}</h3>
+          <h2>{curElement.title}</h2>
+          </div>
+        )
+      })
+      
+    }
+    </div>
     <h1>This is Blog page</h1>
     </>
   )
