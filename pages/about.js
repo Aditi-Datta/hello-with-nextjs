a27deeef97018e0fb8c5ebe87f73b4846b68e85d
Aditi-Datta <aditidatta373@gmail.com>
2023-10-06 14:06:19 +0600

    internal image add

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,12 @@
 import Navbar from "../components/Navbar"
+import Image from "next/image"
+
 function about() {
   return (
     <>
     <Navbar></Navbar>
     <h1>This is About Page Section</h1>
     <p className="introp">Subscribe to my channel</p>
-
     <style jsx>
     {`
       h1{
@@ -14,10 +15,11 @@ function about() {
       .introp{
         color: orange;
       }
-
+      
     `}
     </style>
 
+      <Image src="/computer.jpg" width="500" height="400" ></Image>
 
 
     </>
